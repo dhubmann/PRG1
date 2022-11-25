185b0ba7f6a9cbc72811fcf21a1f77d921569e70
dhubmann <109129228+dhubmann@users.noreply.github.com>
2022-11-25 09:55:14 +0100

    Add files via upload

--- Methods.java
+++ Methods.java
@@ -2,7 +2,7 @@
  * Methods
  * Working with methods
  * Author: Daniel Hubmann
- * Last Change: 09.11.2022
+ * Last Change: 23.11.2022
  */
 
 import java.util.Scanner;
@@ -32,24 +32,27 @@ public class Methods {
 	}
 
 	// Determines minimum between two integers
-	public static int minFunction(int a, int b) {
-		return (a < b) ? a : b;
+	public static int min(int a, int b) {
+		return a < b ? a : b;
 	}
 
 	// Determines maximum between two integers
-	public static int maxFunction(int a, int b) {
-		return (a > b) ? a : b;
+	public static int max(int a, int b) {
+		return a > b ? a : b;
 	}
 
 	// Calculates sum of two integers
-	public static int sumFunction(int a, int b) {
-		return a += b;
+	public static int sum(int a, int b) {
+		return a + b;
 	}
 
-	// Calculates sum from first to second integer including both integers
+	/*
+	 * Calculates sum of all numbers from first to second integer including both
+	 * integers
+	 */
 	public static int sumFromTo(int a, int b) {
-		int min = minFunction(a, b);
-		int max = maxFunction(a, b);
+		int min = min(a, b);
+		int max = max(a, b);
 		int sumFromTo = 0;
 		for (int i = min; i <= max; i++) {
 			sumFromTo += i;
@@ -58,70 +61,101 @@ public class Methods {
 	}
 
 	// Calculates delta between two integers
-	public static int deltaFunction(int a, int b) {
-		int min = minFunction(a, b);
-		int max = maxFunction(a, b);
+	public static int delta(int a, int b) {
+		int min = min(a, b);
+		int max = max(a, b);
 		return max - min;
 	}
 
 	// Calculates mean of two integers
-	public static double meanFunction(int a, int b) {
-		return (a + b) / 2.0;
+	public static float mean(int a, int b) {
+		return (a + b) / 2.0f;
 	}
 
-	// Calculates greatest common divider between two integers
-	public static int gcdFunction(int a, int b) {
-		int min = minFunction(a, b);
-		int max = maxFunction(a, b);
-		int remainder = max % min;
-		while (remainder != 0) {
-			max = min;
-			min = remainder;
-			remainder = max % min;
+	// Calculates greatest common divisor between two integers
+	public static int greatestCommonDivisor(int a, int b) {
+		int min = min(a, b);
+		int max = max(a, b);
+		int result = 1;
+		while (result != 0) {
+			result = max - min;
+			max = max(result, min);
+			min = min(result, min);
 		}
-		return min;
+		return max;
 	}
 
 	// Calculates least common multiple between two integers
-	public static int lcmFunction(int a, int b) {
-		int lcd = (a * b) / gcdFunction(a, b);
+	public static int leastCommonMultiple(int a, int b) {
+		int lcd = (a * b) / greatestCommonDivisor(a, b);
 		return lcd;
 	}
 
-	///////////////////////////////////////////////////////////////////////////////
-	//
-	//
-	//
-	///////////////////////////////////////////////////////////////////////////////
+	// Checks if the given value is a prime number
+	public static boolean isPrime(int input) {
+		if (input == 1) {
+			return false;
+		}
+		if (input == 2 || input == 3) {
+			return true;
+		}
+		for (int factor = 2; factor <= input / 2; factor++) {
+			if (input % factor == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-	// Calculates all prime numbers between two integers
-	// 1 is not prime
-	// return type should probably be an object
-	// since we need to be able to return varying amounts of integers
+	// Prints prime numbers within a range of two integers including both integers
+	public static void primes(int a, int b) {
+		int start = min(a, b);
+		int end = max(a, b);
+		int primeCount = 0;
+		int commaCount = 0;
+		// Determines amount of prime numbers (and commas) for print format
+		for (int i = start; i <= end; i++) {
+			if (isPrime(i)) {
+				primeCount++;
+			}
+		}
+		commaCount = primeCount - 1;
+		// Print statement
+		String result = "primes(" + start + "," + end + ") = {";
+		System.out.print(result);
+		for (int i = start; i <= end; i++) {
+			if (isPrime(i)) {
+				System.out.print(i);
+				if (commaCount != 0) {
+					System.out.print(", ");
+					commaCount--;
+				}
+			}
+		}
+		System.out.print("}");
+	}
 
 	public static void main(String[] args) {
 
 		java.util.Locale.setDefault(new java.util.Locale("en", "US"));
 
-		// Create scanner variable in main method
-		// Use scanner variable as parameter in methods
 		Scanner scanner = new Scanner(System.in);
 
 		int num1 = readNumber(scanner);
 		int num2 = readNumber(scanner);
 
-		minFunction(num1, num2);
-		maxFunction(num1, num2);
+		min(num1, num2);
+		max(num1, num2);
 
-		// Print statements
-		System.out.printf("min(%d,%d) = %d\n", num1, num2, minFunction(num1, num2));
-		System.out.printf("max(%d,%d) = %d\n", num1, num2, maxFunction(num1, num2));
-		System.out.printf("sum(%d,%d) = %d\n", num1, num2, sumFunction(num1, num2));
+		System.out.printf("min(%d,%d) = %d\n", num1, num2, min(num1, num2));
+		System.out.printf("max(%d,%d) = %d\n", num1, num2, max(num1, num2));
+		System.out.printf("sum(%d,%d) = %d\n", num1, num2, sum(num1, num2));
 		System.out.printf("sumFromTo(%d,%d) = %d\n", num1, num2, sumFromTo(num1, num2));
-		System.out.printf("delta(%d,%d) = %d\n", num1, num2, deltaFunction(num1, num2));
-		System.out.printf("mean(%d,%d) = %.2f\n", num1, num2, meanFunction(num1, num2));
-		System.out.printf("gcd(%d,%d) = %d\n", num1, num2, gcdFunction(num1, num2));
-		System.out.printf("lcm(%d,%d) = %d\n", num1, num2, lcmFunction(num1, num2));
+		System.out.printf("delta(%d,%d) = %d\n", num1, num2, delta(num1, num2));
+		System.out.printf("mean(%d,%d) = %.2f\n", num1, num2, mean(num1, num2));
+		System.out.printf("gcd(%d,%d) = %d\n", num1, num2, greatestCommonDivisor(num1, num2));
+		System.out.printf("lcm(%d,%d) = %d\n", num1, num2, leastCommonMultiple(num1, num2));
+		primes(num1, num2);
 
 		scanner.close();
 	}
---
+++ Point.java
@@ -0,0 +1,36 @@
+
+public class Point {
+
+	public int x, y;
+	
+	// Setter for x coordinate
+	public void setX (int x) {
+		this.x = x;
+	}
+	
+	// Getter for x coordinate
+	public int getX () {
+		return x;
+	}
+	
+	// Setter for y coordinate
+	public void setY (int y) {
+		this.y = y;
+	}
+	
+	// Getter for y coordinate
+	public int getY () {
+		return y;
+	}
+	
+	// Constructor
+	public Point() {
+		
+	}
+	
+	// Constructor
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
